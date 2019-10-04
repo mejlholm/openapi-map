ff519e6bddde795c7f41eb93974195eaaf87ea19
mejlholm <mejlholm@mejlholm.org>
2019-10-04 21:45:39 +0200

    Revert "WIP: Added Angular frontend with material design."
    This reverts commit 099f6eac

--- src/main/angular/src/app/models/path-result.model.ts
+++
@@ -1,7 +0,0 @@
-export class PathResult {
-  name: string;
-  openapiUrl: string;
-  openapiUiUrl: string;
-  path: string;
-  operations: string[];
-}
--- src/main/java/org/mejlholm/OpenApiMapApp.java
+++
@@ -1,8 +0,0 @@
-package org.mejlholm;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class OpenApiMapApp extends Application {
-}
