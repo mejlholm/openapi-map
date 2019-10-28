5129ddc437d9b15b3fc4d4caf03a78cf723978d4
Arne Mejlholm <amj@sparnord.dk>
2019-10-28 14:36:43 +0100

    Make it possible to configure logging format.

--- src/main/java/org/mejlholm/LoggerBean.java
+++ src/main/java/org/mejlholm/LoggerBean.java
@@ -3,6 +3,7 @@ package org.mejlholm;
 import io.quarkus.runtime.StartupEvent;
 import io.quarkus.runtime.logging.InitialConfigurator;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logmanager.EmbeddedConfigurator;
 import org.jboss.logmanager.formatters.JsonFormatter;
 import org.jboss.logmanager.handlers.ConsoleHandler;
@@ -17,13 +18,21 @@ import java.util.logging.Level;
 @Slf4j
 public class LoggerBean {
 
+    @ConfigProperty(name = "NAMESPACE", defaultValue = "basic")
+    String logFormat;
+
     void onStart(@Observes StartupEvent ev) {
-        ArrayList<Handler> handlers = new ArrayList(2);
-        Formatter formatter = new JsonFormatter();
-        ConsoleHandler handler = new ConsoleHandler(formatter);
-        handler.setLevel(Level.INFO);
-        handlers.add(handler);
-        InitialConfigurator.DELAYED_HANDLER.setHandlers((Handler[]) handlers.toArray(EmbeddedConfigurator.NO_HANDLERS));
-        log.info("The application is starting...");
+
+        if (logFormat.equalsIgnoreCase("json")) {
+            ArrayList<Handler> handlers = new ArrayList(2);
+            Formatter formatter = new JsonFormatter();
+            ConsoleHandler handler = new ConsoleHandler(formatter);
+            handler.setLevel(Level.INFO);
+            handlers.add(handler);
+            InitialConfigurator.DELAYED_HANDLER.setHandlers((Handler[]) handlers.toArray(EmbeddedConfigurator.NO_HANDLERS));
+            log.info("Logging set to json format");
+        } else {
+            log.info("Logging set to basic format");
+        }
     }
 }
